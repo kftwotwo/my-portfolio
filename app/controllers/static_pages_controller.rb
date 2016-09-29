b498c8467554c57cc43522d930a877cc03764ffd
Duane M. Moody <dmmoody@gmail.com>
2016-09-28 19:41:55 -0700

    WIP contact form work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,21 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to :root
+    else
+      render :contact
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:firstname, :lastname, :email, :subject, :message)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contact
   root to: "static_pages#index"
   get "/contact" => "static_pages#contact"
 end

bbf60a59e3e088445baf8e85f5cdfb1c5deb5339
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-06 19:24:43 -0700

    add flash messages and button

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,9 +9,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      flash[:success] = "Your email was sent!"
       ContactNotifier.send_email(@contact).deliver
       redirect_to root_url
     else
+      flash[:error] = "You did not filled out the form correctly, try again."
       render :new
     end
   end
